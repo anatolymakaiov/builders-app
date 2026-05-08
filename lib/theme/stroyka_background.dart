403efb3c180f3027662cdd00eeb1b6a777ca1cb5
Makaiov Anatolii <makaiov.anatolii@macbook-air.home>
2026-05-08 14:24:29 +0100

    Remove blueprint grid from app backgrounds

--- lib/theme/stroyka_background.dart
+++ lib/theme/stroyka_background.dart
@@ -40,54 +40,10 @@ class StroykaBackground extends StatelessWidget {
             ),
           ),
         ),
-        const _BlueprintGridOverlay(),
         child,
       ],
     );
   }
-}
-
-class _BlueprintGridOverlay extends StatelessWidget {
-  const _BlueprintGridOverlay();
-
-  @override
-  Widget build(BuildContext context) {
-    return IgnorePointer(
-      child: CustomPaint(
-        painter: _BlueprintGridPainter(),
-        child: const SizedBox.expand(),
-      ),
-    );
-  }
-}
-
-class _BlueprintGridPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    final minor = Paint()
-      ..color = AppColors.blueprintLine.withValues(alpha: 0.035)
-      ..strokeWidth = 0.7;
-    final major = Paint()
-      ..color = AppColors.blueprintLine.withValues(alpha: 0.07)
-      ..strokeWidth = 0.8;
-
-    const step = 24.0;
-    for (var x = 0.0; x < size.width; x += step) {
-      canvas.drawLine(Offset(x, 0), Offset(x, size.height), minor);
-    }
-    for (var y = 0.0; y < size.height; y += step) {
-      canvas.drawLine(Offset(0, y), Offset(size.width, y), minor);
-    }
-    for (var x = 0.0; x < size.width; x += step * 4) {
-      canvas.drawLine(Offset(x, 0), Offset(x, size.height), major);
-    }
-    for (var y = 0.0; y < size.height; y += step * 4) {
-      canvas.drawLine(Offset(0, y), Offset(size.width, y), major);
-    }
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
 
 class StroykaSurface extends StatelessWidget {
